725b8d73799a93f3f44cfd87f68c2410bd0c145d
NONHIGH <angelones_24@hotmail.com>
2023-01-06 18:06:22 -0300

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practicaCalculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultadoSuma:number=0;
+  resultadoResta:number=0;
+  resultadoMultiplicacion:number=0;
+  resultadoDivision:number=0;
+
+
+
+  suma():void
+    {
+      this.resultadoSuma = this.numero1+this.numero2;
+    }
+
+    resta():void
+    {
+      this.resultadoResta = this.numero1-this.numero2;
+    }
+    multiplicacion():void
+    {
+      this.resultadoMultiplicacion = this.numero1*this.numero2;
+    }
+    division():void
+    {
+      this.resultadoDivision = this.numero1/this.numero2;
+    }
 }
